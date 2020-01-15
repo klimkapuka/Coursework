6c7d6c20730cb5d9b5870eb79418db7e1ded29e0
klimkapuka <88617@farnborough.ac.uk>
2020-01-15 10:05:17 +0000

    Login finished, started register

---
+++ target/classes/client/js/login.js
@@ -0,0 +1,65 @@
+function pageLoad() {
+
+    if(window.location.search === '?logout') {
+        document.getElementById('content').innerHTML = '<h1>Logging out, please wait...</h1>';
+        logout();
+    } else {
+        document.getElementById("loginButton").addEventListener("click", login);
+        document.getElementById("registerButton").addEventListener("click", register);
+    }
+
+
+}
+
+function login(event) {
+
+    event.preventDefault();
+
+    const form = document.getElementById("loginForm");
+    const formData = new FormData(form);
+
+    fetch("/user/login", {method: 'post', body: formData}
+    ).then(response => response.json()
+    ).then(responseData => {
+
+        if (responseData.hasOwnProperty('error')) {
+            alert(responseData.error);
+        } else {
+            Cookies.set("username", responseData.username);
+            Cookies.set("token", responseData.token);
+
+            window.location.href = '/client/index.html';
+        }
+    });
+}
+function register(event) {
+
+    event.preventDefault();
+
+    const form = document.getElementById("loginForm");
+    const formData = new FormData(form);
+
+}
+
+function logout() {
+
+    fetch("/user/logout", {method: 'post'}
+    ).then(response => response.json()
+    ).then(responseData => {
+        if (responseData.hasOwnProperty('error')) {
+
+            alert(responseData.error);
+
+        } else {
+
+            Cookies.remove("username");
+            Cookies.remove("token");
+
+            window.location.href = '/client/index.html';
+
+        }
+    });
+
+}
+
+
