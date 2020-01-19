6345fe3cf83cc2719c30b5d8800d99c22c9366b9
Klim <88617@farnborough.ac.uk>
2020-01-19 16:57:11 +0000

    Register page working

---
+++ target/classes/client/js/register.js
@@ -0,0 +1,25 @@
+function pageLoad() {
+    document.getElementById("registerButton").addEventListener("click", register);
+}
+
+function register(event) {
+    event.preventDefault();
+
+    const form = document.getElementById("registerForm");
+    const formData = new FormData(form);
+
+    fetch("/user/create", {method: 'post', body: formData}
+    ).then(response => response.json()
+    ).then(responseData => {
+
+        if (responseData.hasOwnProperty('error')) {
+            alert(responseData.error);
+        } else {
+            document.getElementById("content").innerHTML = '<h1 id="successful">User successfully created!</h1>';
+            setTimeout(function (){
+                window.location.href = "login.html"
+            }, 3000);
+
+        }
+});
+}
