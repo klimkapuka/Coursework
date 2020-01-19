326c07b237e84f051e6c33e32722e59c1106e8e0
Klim <88617@farnborough.ac.uk>
2020-01-19 18:51:43 +0000

    index almost finished

--- target/classes/client/js/index.js
+++ target/classes/client/js/index.js
@@ -1,16 +1,11 @@
 function pageLoad() {
 
-    let now = new Date();
-
-    let myHTML = '<div style="text-align:center;">'
-        + '<h1>Welcome to my API powered website!</h1>'
-        + '<img src="/client/img/logo.jpg"  alt="Logo"/>'
-        + '<div style="font-style: italic;">'
-        + 'Generated at ' + now.toLocaleTimeString()
-        + '</div>'
-        + '</div>';
-
-    document.getElementById("testDiv").innerHTML = myHTML;
+    let modal = document.getElementById('modal1');
+    window.onclick = function (event) {
+        if  (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
 
 }
 
@@ -18,37 +13,12 @@ function checkLogin() {
 
     let username = Cookies.get("username");
 
-    let logInHTML = '';
-
     if (username === undefined) {
-
-        let editButtons = document.getElementsByClassName("editButton");
-        for (let button of editButtons) {
-            button.style.visibility = "hidden";
-        }
-
-        let deleteButtons = document.getElementsByClassName("deleteButton");
-        for (let button of deleteButtons) {
-            button.style.visibility = "hidden";
-        }
-
-        logInHTML = "Not logged in. <a href='/client/login.html'>Log in</a>";
+        return false;
     } else {
-
-        let editButtons = document.getElementsByClassName("editButton");
-        for (let button of editButtons) {
-            button.style.visibility = "visible";
-        }
-
-        let deleteButtons = document.getElementsByClassName("deleteButton");
-        for (let button of deleteButtons) {
-            button.style.visibility = "visible";
-        }
-
-        logInHTML = "Logged in as " + username + ". <a href='/client/login.html?logout'>Log out</a>";
-
+        return true;
     }
-
-    document.getElementById("loggedInDetails").innerHTML = logInHTML;
-
 }
+
+
+
