36ba0f903af5037d23095e9cb228a96ef2774761
klimkapuka <88617@farnborough.ac.uk>
2020-01-24 12:14:36 +0000

    Chart Works

--- resources/client/js/weights.js
+++ resources/client/js/weights.js
@@ -1,61 +1,40 @@
 function pageLoad() {
 
-    /*fetch("/weight/track", {method: 'post'}
-    ).then(response => response.json()
-    ).then(responseData => {
+    google.load("visualization", "1", {packages: ["corechart"]});
+    google.setOnLoadCallback(function () {
+        drawChart()
+    });
 
-        if (responseData.hasOwnProperty('error')) {
-            alert(responseData.error);
-        } else {
-            var result = JSON.parse(responseData);
-            drawChart(result);
+    let dates = [];
+    let weights = [];
+
+    fetch("/weight/track", {method: 'post'}
+    ).then(response => response.json()
+    ).then(results => {
+
+        for (let result of results) {
+            dates.push(result.DateRecorded);
+            weights.push(result.CurrentWeight);
         }
     });
 
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+        let data = new google.visualization.DataTable();
+        data.addColumn('string', 'DateRecorded');
+        data.addColumn('number', 'CurrentWeight');
 
-    function drawChart(result) {
-
-        var data = new google.visualization.DataTable();
-            data.addColumn('number', 'CurrentWeight');
-            data.addColumn('string', 'DateRecorded');
-
-        for (var i = 0; i < result.length; i++) {
-            data.addRow([result[i].CurrentWeight, result[i].DateRecorded]);
+        for (let i = 0; i < weights.length; i++) {
+            data.addRow([dates[i], weights[i]]);
         }
 
-        var options = {
-            title: 'Progress Tracker',
-            legend: { position: 'bottom' }
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('chart'));
-
-        chart.draw(data, options);
-
-    }*/
-
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-            ['Date', 'Weight'],
-            ['10/09/2019',  95],
-            ['12/09/2019',  92.5],
-            ['17/09/2019',  90],
-            ['21/09/2019',  85.4]
-        ]);
 
         var options = {
             title: 'Progress Tracker',
-            legend: { position: 'bottom' }
+            legend: {position: 'bottom'}
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('chart'));
+        let chart = new google.visualization.LineChart(document.getElementById('chart'));
 
         chart.draw(data, options);
     }
-
 }
